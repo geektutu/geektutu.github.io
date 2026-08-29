a5901565357ae9ad9dd1bffb76b23eb44b694fe6
gzdaijie <gzdaijie@163.com>
2026-08-30 00:47:21 +0800

    feat: 拉取 geekagent 书籍与文章，忽略其入库目录

--- pull.sh
+++ pull.sh
@@ -18,3 +18,8 @@ cd $root
 pull geektutu/7days-golang posts/7days-golang
 pull geektutu/interview-questions posts/interview-questions
 pull geektutu/high-performance-go posts/high-performance-go
+# 按照 astro 新格式的仓库
+pull geektutu/7days-geekagent posts/7days-geekagent
+mkdir -p src/content/posts/geekagent
+find posts/7days-geekagent -name "geekagent-*.md" | xargs -I{} cp {} src/content/posts/geekagent
+find posts/7days-geekagent -name "geekagent.md" | xargs -I{} cp {} src/content/books
